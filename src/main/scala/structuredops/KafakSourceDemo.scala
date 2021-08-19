2b7f205f9bb88f275f2e1afd434e6bbee7a67e30
aLivableDedode <535074817@qq.com>
2021-08-19 11:57:01 +0800

    #上传代码

--- src/main/scala/structuredops/KafakSourceDemo.scala
+++ src/main/scala/structuredops/KafakSourceDemo.scala
@@ -2,7 +2,10 @@ package structuredops
 
 object KafakSourceDemo {
   def main(args: Array[String]): Unit = {
-
+    val name = System.getProperty("os.name").toUpperCase()
+    val arch = System.getProperty("os.arch").toUpperCase()
+    val version = System.getProperty("os.version").toUpperCase()
+    println(s"$name + $arch + $version" )
   }
 
 }
